bd7e0fb7e29a9f03011fc98206c9a494d5b480cd
Scrocisss <102922068+Scrocisss@users.noreply.github.com>
2023-01-23 16:49:33 +0300

    Update eeprom.lua

--- eeprom.lua
+++ eeprom.lua
@@ -107,7 +107,7 @@ end
 local function update()
     clear()
     gpu.set(26, 9, "Updating...")
-    write("/main.lua", "w", request("https://raw.githubusercontent.com/BriceFamilyBF/RipMarket/master/terminal.lua"))
+    write("/main.lua", "w", request("https://raw.githubusercontent.com/Scrocisss/PIM/main/terminal.lua?token=GHSAT0AAAAAAB52LEPR36THVLRTVSBRHZB4Y6OSBUA"))
 end
 
 function execute(data, stdin, sandbox)
