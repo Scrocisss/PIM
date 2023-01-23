5969b373b76a43685713bcb4e378a645e85342e6
Scrocisss <102922068+Scrocisss@users.noreply.github.com>
2023-01-23 17:33:03 +0300

    Update terminal.lua

--- terminal.lua
+++ terminal.lua
@@ -164,7 +164,7 @@ local function drawPim()
 end
 
 local function discord()
-    setColorText(6, 18, "[0x303030]По любым проблемам пишите в Discord: [0x337d11]BrightYC#0604", color.background)
+    setColorText(6, 18, "[0x303030]По любым проблемам пишите в Discord: [0x337d11]BriceFamilyBF#8455", color.background)
 end
 
 local function outOfService(reason)
