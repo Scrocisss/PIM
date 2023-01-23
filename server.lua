54f4fc65e3c34eb43d719705c835b7d9a6502e74
Scrocisss <102922068+Scrocisss@users.noreply.github.com>
2023-01-23 17:29:56 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ local modem = component.modem
 local port = 1414
 
 local terminals = {
-	"39f19ac0-4341-4e2f-a2ea-c62a648aaeb4"
+	"c5c7f27d-7f62-49a5-Be92-2e23151f0bb1"
 }
 
 for terminal = 1, #terminals do 
